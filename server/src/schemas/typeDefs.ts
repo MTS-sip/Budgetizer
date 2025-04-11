d341b6e5d3cbd192685ef9eee9306d202b0e45d4
jaegerthomas <bjaegerthomas@gmail.com>
2025-04-10 21:27:05 -0400

    updating placholder files for schemas

--- server/src/schemas/typeDefs.ts
+++ server/src/schemas/typeDefs.ts
@@ -1,4 +1,42 @@
-const typeDefs = `
- 
+import { gql } from 'graphql-tag';
+
+const typeDefs = gql`
+
+  type Subcategory {
+    name: String!
+    amount: Float!
+  }
+
+  type BudgetCategory {
+    category: String!
+    subcategories: [Subcategory!]!
+  }
+
+  type User {
+    _id: ID!
+    username: String!
+    password: String!
+    budget: [BudgetCategory!]!
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+
+  type Query {
+    me: User
+  }
+
+  input SubcategoryInput {
+    name: String!
+    amount: Float!
+  }
+
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    updateSubcategory(username: String!, input: SubcategoryInput!): User
+  }
+`;
 
 export default typeDefs;
