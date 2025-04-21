7c7568ca2f996a43416fc0cd429a75e0425292e6
jaegerthomas <bjaegerthomas@gmail.com>
2025-04-21 19:33:22 -0400

    changed input boxes for login to smaller size

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -35,6 +35,7 @@ const Login: React.FC = () => {
         label="Username"
         name="username"
         type="text"
+        size="small"
         placeholder="Enter your username"
         value={formState.username}
         onChange={handleChange}
@@ -44,6 +45,7 @@ const Login: React.FC = () => {
         label="Password"
         name="password"
         type="text"
+        size="small"
         value={formState.password}
         onChange={handleChange}
         required
