b76a108fa5447dcbdc98f48bf34466897638bd02
M Possible <185700337+MTS-sip@users.noreply.github.com>
2025-04-22 13:21:24 -0400

    cors4

--- server/src/server.ts
+++ server/src/server.ts
@@ -22,7 +22,7 @@ const startApolloServer = async () => {
   await server.start();
   await db();
 
-  const PORT = process.env.PORT || 5001;
+  const PORT = process.env.PORT || 3050;
   const app = express();
 
   app.use(express.urlencoded({ extended: false }));
