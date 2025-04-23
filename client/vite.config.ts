c67beb1c397a8ae1e5ce21c73a87b3e0a9ba80f3
M Possible <185700337+MTS-sip@users.noreply.github.com>
2025-04-23 14:21:50 -0400

    $ amount render, reveal function, merger forker repo with Team repo

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite';
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    port: 3030,
+    port: 3080,
     open: true,
     proxy: {
   // We have to set up our Vite client-side development server to proxy API requests to our server-side Node server at port 3001 in order to hit our GraphQL application.
