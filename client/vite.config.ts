c041861ca6f92621bbf5e27ef758c4408aada349
M Possible <185700337+MTS-sip@users.noreply.github.com>
2025-04-23 22:28:24 -0400

    502 error

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
     proxy: {
   // We have to set up our Vite client-side development server to proxy API requests to our server-side Node server at port 3001 in order to hit our GraphQL application.
       '/graphql': {
-        target: 'http://localhost:3001',
+        target: 'http://localhost:5001',
         changeOrigin: true,
         secure: false,
       },
